b13c179e85c7e5582ec2c3b3734768153d5b0378
Jenny Belanger <jbelangr@gmail.com>
2022-03-31 21:14:32 -0400

    Convert spaces to tabs

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
--- routes/channels.php
+++ routes/channels.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+	return (int) $user->id === (int) $id;
 });
