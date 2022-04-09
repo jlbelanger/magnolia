3eb70a8ed22d1339f380fe1f90255f38a338a4bc
Jenny Belanger <jbelangr@gmail.com>
2022-04-08 21:56:15 -0400

    Prevent notes overlapping content

--- resources/js/toggleable.js
+++ resources/js/toggleable.js
@@ -2,6 +2,10 @@ function Toggleable($button) {
 	$button.addEventListener('click', (e) => {
 		const selector = e.target.getAttribute('data-toggleable');
 		document.querySelector(selector).classList.toggle('show');
+		const className = e.target.getAttribute('data-toggleable-body-class');
+		if (className) {
+			document.body.classList.toggle(className);
+		}
 	});
 }
 
--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -5,7 +5,7 @@
 
 	@if (Auth::user())
 		<a class="button admin" href="/recipes/{{ $row->id }}/edit" id="edit-button">Edit</a>
-		<button aria-label="Add Note" class="admin" data-toggleable="#note-form" id="add-note-button" type="button">+</button>
+		<button aria-label="Add Note" class="admin" data-toggleable="#note-form" data-toggleable-body-class="show-note" id="add-note-button" type="button">+</button>
 		<form action="/recipes/{{ $row->id }}" data-ajax id="note-form" method="post">
 			@csrf
 			@method('PUT')
