f3823369b558af449b6b814f16dd3860778a6e00
Jenny Belanger <jbelangr@gmail.com>
2022-07-04 21:36:26 -0400

    Clean up JS

--- resources/js/password.js
+++ resources/js/password.js
@@ -1,21 +1,29 @@
-function Password($button) {
-	$button.addEventListener('click', (e) => {
+function Passwordable($button) {
+	const onClick = (e) => {
 		const $input = e.target.closest('.password-container').querySelector('.password-input');
 		if ($input.getAttribute('type') === 'password') {
 			$input.setAttribute('type', 'text');
+			$input.setAttribute('aria-label', 'Hide Password');
 			$button.innerText = 'Hide';
 		} else {
 			$input.setAttribute('type', 'password');
+			$input.setAttribute('aria-label', 'Show Password');
 			$button.innerText = 'Show';
 		}
-	});
+	};
+
+	const init = () => {
+		$button.addEventListener('click', onClick);
+	};
+
+	init();
 }
 
-function initPassword() {
+function initPasswordable() {
 	const $elements = document.querySelectorAll('[data-toggle-password]');
 	$elements.forEach(($element) => {
-		Password($element);
+		Passwordable($element);
 	});
 }
 
-initPassword();
+initPasswordable();
--- resources/js/toggleable.js
+++ resources/js/toggleable.js
@@ -1,12 +1,18 @@
 function Toggleable($button) {
-	$button.addEventListener('click', (e) => {
-		const selector = e.target.getAttribute('data-toggleable');
+	const onClick = () => {
+		const selector = $button.getAttribute('data-toggleable');
 		document.querySelector(selector).classList.toggle('show');
-		const className = e.target.getAttribute('data-toggleable-body-class');
+		const className = $button.getAttribute('data-toggleable-body-class');
 		if (className) {
 			document.body.classList.toggle(className);
 		}
-	});
+	};
+
+	const init = () => {
+		$button.addEventListener('click', onClick);
+	};
+
+	init();
 }
 
 function initToggleable() {
