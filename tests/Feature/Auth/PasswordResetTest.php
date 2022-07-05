62de933800e63b9f86ddda995f6ffab8aa12f8b9
Jenny Belanger <jbelangr@gmail.com>
2022-07-04 21:42:08 -0400

    Fix test

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -59,8 +59,8 @@ class PasswordResetTest extends TestCase
 			$response = $this->post('/reset-password', [
 				'token' => $notification->token,
 				'email' => $user->email,
-				'new_password' => 'password',
-				'new_password_confirmation' => 'password',
+				'password' => 'password',
+				'password_confirmation' => 'password',
 			]);
 
 			$response->assertSessionHasNoErrors();
