d865d9d47ed31dc6222d1481580c6e0bc68e5ead
Jenny Belanger <jbelangr@gmail.com>
2022-07-04 21:34:33 -0400

    Fix resetting password

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -37,17 +37,17 @@ class NewPasswordController extends Controller
 		$request->validate([
 			'token' => ['required'],
 			'email' => ['required', 'email'],
-			'new_password' => ['required', 'confirmed', Rules\Password::defaults()],
+			'password' => ['required', 'confirmed', Rules\Password::defaults()],
 		]);
 
 		// Here we will attempt to reset the user's password. If it is successful we
 		// will update the password on an actual user model and persist it to the
 		// database. Otherwise we will parse the error and return the response.
 		$message = Password::reset(
-			$request->only('email', 'new_password', 'new_password_confirmation', 'token'),
+			$request->only('email', 'password', 'password_confirmation', 'token'),
 			function ($user) use ($request) {
 				$user->forceFill([
-					'new_password' => Hash::make($request->new_password),
+					'password' => Hash::make($request->password),
 					'remember_token' => Str::random(60),
 				])->save();
 
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -19,22 +19,31 @@
 		<p>
 			<label class="required" for="email">Email</label>
 			<input autocomplete="email" id="email" name="email" required type="email" value="{{ old('email', $request->email) }}" />
+			@error('email')
+				<span class="error">{{ $message }}</span>
+			@enderror
 		</p>
 
 		<p>
 			<label class="required" for="password">New password</label>
 			<span class="password-container">
-				<input autocomplete="new-password" autofocus class="password-input" id="password" name="password" required type="password" />
+				<input autocomplete="new-password" autocorrect="off" autofocus class="password-input" id="password" name="password" required type="password" />
 				<button class="password-button" data-toggle-password type="button">Show</button>
 			</span>
+			@error('password')
+				<span class="error">{{ $message }}</span>
+			@enderror
 		</p>
 
 		<p>
-			<label class="required" for="new_password_confirmation">Confirm new password</label>
+			<label class="required" for="password_confirmation">Confirm new password</label>
 			<span class="password-container">
-				<input autocomplete="new-password" class="password-input" id="new_password_confirmation" name="new_password_confirmation" required type="password" />
+				<input autocomplete="new-password" autocorrect="off" class="password-input" id="password_confirmation" name="password_confirmation" required type="password" />
 				<button class="password-button" data-toggle-password type="button">Show</button>
 			</span>
+			@error('password_confirmation')
+				<span class="error">{{ $message }}</span>
+			@enderror
 		</p>
 
 		<p>
