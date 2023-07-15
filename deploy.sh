84ed9f649160e3e2efe0b90fe8b3b5c0f5d02ca7
Jenny Belanger <jbelangr@gmail.com>
2023-07-15 15:02:40 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,11 @@ source "${HOME}/Websites/infrastructure/deploy/static.sh"
 source "${HOME}/Websites/infrastructure/deploy/laravel.sh"
 
 check_git_branch
-check_git_changes
 build_static
+check_git_changes
 deploy_git
 deploy_composer
 deploy_laravel
 clear_laravel_cache
 create_uploads_folder
+printf "\e[0;32mDone.\n\e[0m"
