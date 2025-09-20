4958685508cf0111285ec9a5fdad44c7c4a03fbd
Jenny Belanger <jbelangr@gmail.com>
2025-09-19 21:16:25 -0400

    Change minutes from tinyint to smallint

---
+++ database/migrations/2025_09_20_011432_change_times_minutes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Runs the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() : void
+	{
+		Schema::table('times', function (Blueprint $table) {
+			$table->unsignedSmallInteger('minutes')->change();
+		});
+	}
+
+	/**
+	 * Reverses the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() : void
+	{
+		Schema::table('times', function (Blueprint $table) {
+			$table->tinyInteger('minutes')->change();
+		});
+	}
+};
